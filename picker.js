123c75980302f3dd55c7fa5914ef561463e67cf3
Andrew Taylor <mac@andrewt.net>
2015-05-15 21:06:09 +0100

    This was never useful.

--- picker.js
+++ picker.js
@@ -7,8 +7,7 @@ Picker.prototype.push = function(key) {
 	this.members[key] = this.count(key) + 1;
 };
 Picker.prototype.pick = function() {
-	var i = 0,
-		n = Math.random() * this.totalCount;
+	var n = Math.random() * this.totalCount;
 	for (var key in this.members)
 		if ((n -= this.members[key]) < 0)
 			return key;
