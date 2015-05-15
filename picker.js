664145191b1902c70392924f8bbf31ff324a4af5
Andrew Taylor <mac@andrewt.net>
2015-05-15 20:31:09 +0100

    Removed all dependencies.

--- picker.js
+++ picker.js
@@ -1,13 +1,18 @@
-var deck = require('deck');
-
 function Picker() {
+	this.totalCount = 0;
 	this.members = {};
 }
 Picker.prototype.push = function(key) {
+	++this.totalCount;
 	this.members[key] = this.count(key) + 1;
 };
 Picker.prototype.pick = function() {
-	return deck.pick(this.members);
+	var i = 0,
+		n = Math.random() * this.totalCount;
+	for (var key in this.members)
+		if ((n -= this.members[key]) < 0)
+			return key;
+	throw 'No member found.';
 };
 Picker.prototype.count = function(key) {
 	return this.members[key] || 0;
