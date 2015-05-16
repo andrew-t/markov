e5bf5c51139d54b29d72ff18b72f5934a141dc7b
Andrew Taylor <mac@andrewt.net>
2015-05-16 21:08:11 +0100

    Start on weightings

--- demo.js
+++ demo.js
@@ -1,10 +1,11 @@
 preload(['./markov', './picker', './shelf', './util', './splitter'], function() {
 	var Markov = require('./markov'),
-		m,
+		order,
+		chains = [],
 		$ = document.getElementById.bind(document);
 
 	$('define').addEventListener('click', function() {
-		m = new Markov($('order').value);
+		order = $('order').value;
 		$('1').classList.add('hidden');
 		$('2').classList.remove('hidden');
 		$('corpus').focus();
@@ -12,12 +13,20 @@ preload(['./markov', './picker', './shelf', './util', './splitter'], function() 
 	});
 
 	$('train').addEventListener('click', function() {
+		var m = new Markov(order);
+		chains.push({
+			chain: m,
+			weight: 1
+		});
 		$('corpus').value
 			.split(new RegExp($('split').value, 'g'))
 			.filter(function(x) { return x.trim(); })
 			.forEach(m.train.bind(m));
 		$('corpus').value = '';
 		$('3').classList.remove('hidden');
+		$('4').classList.remove('hidden');
+		
+		list();
 	});
 
 	$('ramble').addEventListener('click', ramble);
@@ -26,7 +35,7 @@ preload(['./markov', './picker', './shelf', './util', './splitter'], function() 
 		ramble();
 	});
 	function ramble() {
-		$('output').value = m.ramble(
+		$('output').value = Markov.combine(chains).ramble(
 			$('output').value || undefined,
 			$('forever').checked
 				? undefined
@@ -40,8 +49,55 @@ preload(['./markov', './picker', './shelf', './util', './splitter'], function() 
 	}
 
 	$('reset').addEventListener('click', function() {
+		chains = [];
 		$('1').classList.remove('hidden');
 		$('2').classList.add('hidden');
 		$('3').classList.add('hidden');
+		$('4').classList.add('hidden');
 	});
+
+	function list() {
+		$('chains').innerHTML = '';
+		chains.forEach(tr);
+	}
+
+	function tr(markov, i) {
+		var id = 'chain-' + i,
+			tr = $(id),
+			wStarters,
+			wChain,
+			total = 0;
+		for (var key in markov.chain.chain)
+			total += markov.chain.chain[key].totalCount;;
+		if (!tr) {
+			var tr = el($('chains'), 'tr', null, { id: id })
+			el(tr, 'td', markov.chain.ramble(null, Math.max(8, order)));
+			el(tr, 'td', markov.chain.starters.totalCount);
+			el(tr, 'td', total);
+			el(el(tr, 'td'), 'input', null, {
+				type: 'number',
+				value: markov.weight,
+				id: id + '-w'
+			}).addEventListener('change', function() {
+				markov.weight = $(id + '-w').value;
+				list();
+			});
+			el(tr, 'td', null, { id: id + '-ws' });
+			el(tr, 'td', null, { id: id + '-wc' });
+		}
+		$(id + '-ws').innerHTML = markov.chain.starters.totalCount * markov.weight
+		$(id + '-wc').innerHTML = total * markov.weight
+		return tr;
+	}
+
+	function el(parent, tag, text, attr) {
+		var el = document.createElement(tag);
+		parent.appendChild(el);
+		if (text)
+			el.appendChild(document.createTextNode(text));
+		if (attr)
+			for (var key in attr)
+				el.setAttribute(key, attr[key]);
+		return el;
+	}
 });
