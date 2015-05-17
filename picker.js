551cc0b36581484d26aa8ec0a723ba81c4ba5b93
Andrew Taylor <mac@andrewt.net>
2015-05-17 13:10:33 +0100

    Safer code and a big fix.

--- picker.js
+++ picker.js
@@ -13,32 +13,33 @@ Picker.prototype.pick = function() {
 	var n = Math.random() * this.totalCount;
 	for (var key in this.members)
 		if (this.members.hasOwnProperty(key) &&
-			((n -= this.members[key]) < 0))
+			((n -= this.count(key)) < 0))
 				return key;
 	throw 'No member found.';
 };
 Picker.prototype.count = function(key) {
-	return this.members[key] || 0;
+	var count = this.members[key];
+	return isNaN(count) ? 0 : count;
 };
 Picker.prototype.inject = function(donor, weight) {
 	if (weight == undefined)
 		weight = 1;
 	for (var key in donor.members)
-		if (this.members.hasOwnProperty(key))
-			this.push(key, donor.members[key] * weight);
+		if (donor.members.hasOwnProperty(key))
+			this.push(key, donor.count(key) * weight);
 	return this;
 };
 Picker.prototype.multiply = function(weight) {
 	for (var key in this.members)
 		if (this.members.hasOwnProperty(key))
-			this.members[key] *= weight;
+			this.members[key] = this.count(key) * weight;
 	return this;
 };
 Picker.prototype.clone = function() {
 	var clone = new Picker();
 	for (var key in this.members)
 		if (this.members.hasOwnProperty(key))
-			clone.push(key, this.members[key]);
+			clone.push(key, this.count(key));
 	return clone;
 };
 
